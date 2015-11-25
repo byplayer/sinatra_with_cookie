231f5762990ad3ce9943720d16821c44e7cbad82
Yukio Goto <yukio.goto@rakuten.com>
2015-11-25 09:57:32 +0800

    implement app

---
+++ app.rb
@@ -0,0 +1,16 @@
+require 'sinatra'
+require 'sinatra/cookies'
+
+get '/' do
+  cookies[:test] = 'micky'
+  'hello'
+end
+
+get '/read' do
+  "cookie[:test]=#{cookies[:test]}"
+end
+
+get '/del_cookie' do
+  response.delete_cookie :test
+  'deleted'
+end
